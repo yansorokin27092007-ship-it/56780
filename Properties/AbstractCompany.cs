c381fa96aca291fc444af4669db880e2f597abb0
Yanik73 <senq73@gmail.com>
2026-05-23 02:23:46 +0400

    Абстрактный класс

---
+++ Properties/AbstractCompany.cs
@@ -0,0 +1,118 @@
+﻿using ProjectDumpTruck.Drawnings;
+
+namespace ProjectDumpTruck.CollectionGenericObjects;
+
+public abstract class AbstractCompany
+{
+    /// <summary> 
+    /// Размер места (ширина) 
+    /// </summary> 
+    protected readonly int _placeSizeWidth;
+
+    /// <summary> 
+    /// Размер места (высота) 
+    /// </summary> 
+    protected readonly int _placeSizeHeight;
+
+    /// <summary> 
+    /// Ширина окна 
+    /// </summary> 
+    protected readonly int _pictureWidth;
+
+    /// <summary> 
+    /// Высота окна 
+    /// </summary> 
+    protected readonly int _pictureHeight;
+
+    /// <summary> 
+    /// Коллекция 
+    /// </summary> 
+    protected ICollectionGenericObjects<DrawningCar> _collection;
+
+    /// <summary> 
+    /// Конструктор 
+    /// </summary> 
+    /// <param name="pictureWidth">Ширина окна</param> 
+    /// <param name="pictureHeight">Высота окна</param> 
+    /// <param name="placeSizeWidth">Размер места (ширина)</param> 
+    /// <param name="placeSizeHeight">Размер места (высота)</param> 
+    /// <param name="collection">Коллекция</param> 
+    public AbstractCompany(int pictureWidth, int pictureHeight, int placeSizeWidth, int placeSizeHeight, ICollectionGenericObjects<DrawningCar> collection)
+    {
+        _pictureWidth = pictureWidth;
+        _pictureHeight = pictureHeight;
+        _placeSizeWidth = placeSizeWidth;
+        _placeSizeHeight = placeSizeHeight;
+        _collection = collection;
+        _collection.MaxCount = CalcMaxCount();
+    }
+
+    /// <summary> 
+    /// Перегрузка оператора сложения для класса 
+    /// </summary> 
+    /// <param name="company">Компания</param> 
+    /// <param name="car">Добавляемый объект</param> 
+    /// <returns>Компания с добавленным объектом</returns> 
+    public static int operator +(AbstractCompany company, DrawningCar car) => company._collection.InsertObject(car);
+
+    /// <summary> 
+    /// Перегрузка оператора удаления для класса 
+    /// </summary> 
+    /// <param name="company">Компания</param> 
+    /// <param name="position">Номер удаляемого объекта</param> 
+    /// <returns>Компания с удалённым объектом</returns> 
+    public static DrawningCar? operator -(AbstractCompany company, int position) => company._collection.RemoveObject(position);
+
+    /// <summary> 
+    /// Получение случайного объекта из коллекции 
+    /// </summary> 
+    /// <returns>Объект из коллекции</returns> 
+    public DrawningCar? GetRandomObject()
+    {
+        Random random = new();
+        int maxCount = CalcMaxCount();
+        DrawningCar? drawningCar = null;
+        int counter = 10;
+        while (drawningCar is null)
+        {
+            drawningCar = _collection.GetObject(random.Next(0, maxCount));
+            counter--;
+            if (counter == 0)
+            {
+                break;
+            }
+        }
+
+        return drawningCar;
+    }
+
+    /// <summary> 
+    /// Вывод всей коллекции 
+    /// </summary> 
+    /// <returns></returns> 
+    public Bitmap? Show()
+    {
+        Bitmap bitmap = new(_pictureWidth, _pictureHeight);
+        Graphics graphics = Graphics.FromImage(bitmap);
+        DrawBackground(graphics);
+        DrawObjects(graphics);
+        return bitmap;
+    }
+
+    /// <summary> 
+    /// Вывод заднего фона 
+    /// </summary> 
+    /// <param name="g"></param> 
+    protected abstract void DrawBackground(Graphics g);
+
+    /// <summary> 
+    /// Расстановка и прорисовка объектов 
+    /// </summary> 
+    /// <param name="g"></param> 
+    protected abstract void DrawObjects(Graphics g);
+
+    /// <summary> 
+    /// Вычисление максимального количества элементов, который можно разместить в окне
+    /// </summary> 
+    private int CalcMaxCount() => (int)(Math.Truncate((double)_pictureWidth / _placeSizeWidth) * Math.Truncate((double)_pictureHeight / _placeSizeHeight));
+}
---
+++ Properties/CarSharingService.cs
@@ -0,0 +1,81 @@
+﻿
+using ProjectDumpTruck.Drawnings;
+using ProjectDumpTruck.CollectionGenericObjects;
+namespace ProjectDumpTruck.CollectionGenericObjects
+{
+    /// <summary>
+    /// Реализация компании-сервис каршеринга (автопарк)
+    /// </summary>
+
+    public class CarSharingService : AbstractCompany
+    {
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="pictureWidth"></param>
+        /// <param name="pictureHeight"></param>
+        /// <param name="collection"></param>
+        public CarSharingService(int pictureWidth, int pictureHeight, ICollectionGenericObjects<DrawningCar> collection) : base(pictureWidth, pictureHeight, 110, 70, collection)
+        {
+
+        }
+        protected override void DrawBackground(Graphics g)
+        {
+            g.Clear(Color.White);
+
+            int cols = _pictureWidth / _placeSizeWidth;
+            int rows = _pictureHeight / _placeSizeHeight;
+            int offset = 40; // Отступ от краев
+
+            using (Pen pen = new Pen(Color.Black, 1))
+            {
+
+                for (int col = 0; col <= cols; col++)
+                {
+                    int x = offset + col * _placeSizeWidth;
+                    g.DrawLine(pen, x, offset, x, offset + rows * _placeSizeHeight);
+                }
+
+                // Рисуем горизонтальные линии с учетом отступа
+                for (int row = 0; row <= rows; row++)
+                {
+                    int y = offset + row * _placeSizeHeight;
+                    g.DrawLine(pen, offset, y, offset + cols * _placeSizeWidth, y);
+                }
+                pen.Dispose();
+            }
+
+        }
+
+        protected override void DrawObjects(Graphics g)
+        {
+            int cols = _pictureWidth / _placeSizeWidth;
+            int rows = _pictureHeight / _placeSizeHeight;
+            int offset = 40;
+
+            for (int i = 0; i < cols * rows; i++)
+            {
+                var obj = _collection.GetObject(i);
+                if (obj == null) continue;
+
+                int col = i % cols;
+                int row = i / cols;
+
+                // Вычисляем координаты ячейки с учетом отступа
+                int cellX = offset + col * _placeSizeWidth;
+                int cellY = offset + row * _placeSizeHeight;
+
+                // Центрируем объект внутри ячейки
+                int x = cellX + (_placeSizeWidth - obj.DrawningCarWidth) / 2;
+                int y = cellY + (_placeSizeHeight - obj.DrawningCarHeight) / 2;
+
+                obj.SetPosition(x, y);
+                obj.DrawTransport(g);
+            }
+        }
+    }
+}
+
+
+
+
